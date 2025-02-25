6dbd2c59d639ef96b2eb8e789e8a17b4bb4d3fa3
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-02-25 13:12:45 -0800

    dssdsaa
    sad

--- scripty.js
+++ scripty.js
@@ -1,21 +1,3 @@
-// alert("start")
-
-// async function main() {
-//     const adapter = await navigator.gpu?.requestAdapter();
-//     const device = await adapter?.requestDevice();
-//     if (!device) {
-//         alert("succeed")
-//       fail('need a browser that supports WebGPU');
-//       return;
-//     } else {
-//         alert("fail")
-//     }
-//   }
-//   main();
-
-//   alert("end")
-
-
   async function main() {
     try {
       const adapter = await navigator.gpu?.requestAdapter();
