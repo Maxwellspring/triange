7faac5a61f20e573b883d63075a5897ba4876d97
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-02-26 12:36:19 -0800

    sw

--- scripty.js
+++ scripty.js
@@ -35,7 +35,7 @@ async function main() {
 
         console.log('WebGPU device successfully obtained:', device);
         alert("webgpu successfully started")
-        // write code here VVVVV
+        // V START OF CODE V
 
         // Get a WebGPU context from the canvas and configure it
         const canvas = document.querySelector('canvas');
@@ -46,7 +46,28 @@ async function main() {
           format: presentationFormat,
         });
 
+        const module = device.createShaderModule({
+          label: 'our hardcoded red triangle shaders',
+          code: `
+            @vertex fn vs(
+              @builtin(vertex_index) vertexIndex : u32
+            ) -> @builtin(position) vec4f {
+              let pos = array(
+                vec2f( 0.0,  0.5),  // top center
+                vec2f(-0.5, -0.5),  // bottom left
+                vec2f( 0.5, -0.5)   // bottom right
+              );
+       
+              return vec4f(pos[vertexIndex], 0.0, 1.0);
+            }
+       
+            @fragment fn fs() -> @location(0) vec4f {
+              return vec4f(1.0, 0.0, 0.0, 1.0);
+            }
+          `,
+        });
 
+        // ^ END OF CODE ^
       } catch (error) {
         alert("and error happened")
         console.error('An error occurred:', error);
