f89aee953f6175b72ab55ba24d59e24ae7010e3d
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-02-26 13:03:50 -0800

    IT DRAWS A TRIANGLE

--- scripty.js
+++ scripty.js
@@ -37,6 +37,15 @@ async function main() {
         alert("webgpu successfully started")
         // V START OF CODE V
 
+
+
+
+
+
+
+
+
+        
         // Get a WebGPU context from the canvas and configure it
         const canvas = document.querySelector('canvas');
         const context = canvas.getContext('webgpu');
@@ -66,6 +75,62 @@ async function main() {
             }
           `,
         });
+
+        const pipeline = device.createRenderPipeline({
+          label: 'our hardcoded red triangle pipeline',
+          layout: 'auto',
+          vertex: {
+            // entryPoint: 'vs',
+            module,
+          },
+          fragment: {
+            // entryPoint: 'fs',
+            module,
+            targets: [{ format: presentationFormat }],
+          },
+        });
+
+        const renderPassDescriptor = {
+          label: 'our basic canvas renderPass',
+          colorAttachments: [
+            {
+              // view: <- to be filled out when we render
+              clearValue: [0.3, 0.3, 0.3, 1],
+              loadOp: 'clear',
+              storeOp: 'store',
+            },
+          ],
+        };
+
+        function render() {
+          // Get the current texture from the canvas context and
+          // set it as the texture to render to.
+          renderPassDescriptor.colorAttachments[0].view =
+              context.getCurrentTexture().createView();
+       
+          // make a command encoder to start encoding commands
+          const encoder = device.createCommandEncoder({ label: 'our encoder' });
+       
+          // make a render pass encoder to encode render specific commands
+          const pass = encoder.beginRenderPass(renderPassDescriptor);
+          pass.setPipeline(pipeline);
+          pass.draw(3);  // call our vertex shader 3 times
+          pass.end();
+       
+          const commandBuffer = encoder.finish();
+          device.queue.submit([commandBuffer]);
+        }
+       
+        render();
+
+
+
+
+
+
+
+
+
 
         // ^ END OF CODE ^
       } catch (error) {
