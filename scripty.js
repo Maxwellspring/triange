d9039d7c38dc0eb2ed6edfb07ebdab3e3eb54cbf
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-02-27 11:42:37 -0800

    yeee

--- scripty.js
+++ scripty.js
@@ -1,3 +1,11 @@
+let numberr = 0.4
+
+function changeStuff() {
+  numberr += 0.1
+  main();
+  return numberr
+}
+
 async function main() {
   try {
     const adapter = await navigator.gpu?.requestAdapter();
@@ -15,7 +23,7 @@ async function main() {
     }
 
     console.log('WebGPU device successfully obtained:', device);
-    alert("webgpu successfully started")
+    alert("WebGPU Device Success")
 
     async function main() {
       try {
@@ -34,7 +42,7 @@ async function main() {
         }
 
         console.log('WebGPU device successfully obtained:', device);
-        alert("webgpu successfully started")
+        alert("WebGPU Device Success")
         // V START OF CODE V
 
 
@@ -95,7 +103,7 @@ async function main() {
           colorAttachments: [
             {
               // view: <- to be filled out when we render
-              clearValue: [0.3, 0.3, 0.3, 1],
+              clearValue: [0.3, 0.3, numberr, 1],
               loadOp: 'clear',
               storeOp: 'store',
             },
@@ -233,7 +241,71 @@ main();
 //           `,
 //         });
 
-        
+//         const pipeline = device.createComputePipeline({
+//           label: 'doubling compute pipeline',
+//           layout: 'auto',
+//           compute: {
+//             module,
+//           },
+//         });
+
+//         const input = new Float32Array([1, 3, 5]);
+
+//         // create a buffer on the GPU to hold our computation
+//         // input and output
+//         const workBuffer = device.createBuffer({
+//           label: 'work buffer',
+//           size: input.byteLength,
+//           usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST,
+//         });
+//         // Copy our input data to that buffer
+//         device.queue.writeBuffer(workBuffer, 0, input);
+
+//         // create a buffer on the GPU to get a copy of the results
+//         const resultBuffer = device.createBuffer({
+//           label: 'result buffer',
+//           size: input.byteLength,
+//           usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST
+//         });
+
+//         // Setup a bindGroup to tell the shader which
+//         // buffer to use for the computation
+//         const bindGroup = device.createBindGroup({
+//           label: 'bindGroup for work buffer',
+//           layout: pipeline.getBindGroupLayout(0),
+//           entries: [
+//             { binding: 0, resource: { buffer: workBuffer } },
+//           ],
+//         });
+
+//         // Encode commands to do the computation
+//         const encoder = device.createCommandEncoder({
+//           label: 'doubling encoder',
+//         });
+//         const pass = encoder.beginComputePass({
+//           label: 'doubling compute pass',
+//         });
+//         pass.setPipeline(pipeline);
+//         pass.setBindGroup(0, bindGroup);
+//         pass.dispatchWorkgroups(input.length);
+//         pass.end();
+
+//         // Encode a command to copy the results to a mappable buffer.
+//         encoder.copyBufferToBuffer(workBuffer, 0, resultBuffer, 0, resultBuffer.size);
+
+//         // Finish encoding and submit the commands
+//         const commandBuffer = encoder.finish();
+//         device.queue.submit([commandBuffer]);
+
+//         // Read the results
+//         await resultBuffer.mapAsync(GPUMapMode.READ);
+//         const result = new Float32Array(resultBuffer.getMappedRange());
+
+//         console.log('input', input);
+//         document.getElementById("fake-console").textContent += `input ${input} `
+//         console.log('result', result);
+//         document.getElementById("fake-console").textContent += `result ${result} `
+//         resultBuffer.unmap();
 
 
 
